534b876e0561e5f9e98521d270a64416e01ca7f7
TouchsadaJanOn <57841675+TouchsadaJanOn@users.noreply.github.com>
2022-01-20 16:46:45 -0700

    add more to program

--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function Welcome(props) {
+  return (
+  <div>
+  <h1>Hello, {props.name}</h1>
+    <h2>Good to see you!</h2>
+    </div>
+    );
+}
+
+function Main(){
+  return (
+    <div>
+      <Welcome name="Touchsada Jan On" />
+      <h2>The time is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+ 
 ReactDOM.render(
-  <h1>Hello, World</h1>,
+  <Main />,
   document.getElementById('root')
 );
 
-
+setInterval(Main, 1000);
