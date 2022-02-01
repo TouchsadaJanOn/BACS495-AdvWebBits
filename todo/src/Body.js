b4da66c59a8f1f41f2f24dc975509a42eed129aa
TouchsadaJanOn <57841675+TouchsadaJanOn@users.noreply.github.com>
2022-02-01 16:41:03 -0700

    log demo

--- todo/src/Body.js
+++ todo/src/Body.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react'
 
 function Body(){
     const [name, setName] = useState();
+    console.log("hey!!!!!!")
     return <div>
         <input value ={name} onInput ={e=> setName(e.target.value)}/>
         <p>Name is set to: {name}</p>
